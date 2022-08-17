c6b198b96c223b2a148cbdfb789c15f0658a23e2
ravindra-me <ravindras@sigmoidanalytics.com>
2022-08-17 10:07:09 +0530

    ADD: initial comment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("hello express kill");
+  res.send("hello express kill2");
 });
 
 module.exports = router;
